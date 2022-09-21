ccba83737f7a98537b35f592d68bbecd44d3c56b
esponges <esponges@gmail.com>
2022-09-21 17:09:42 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { ReduxToolKit } from './components/ReduxToolKit';
-import { store } from './stores/redux-tool-kit/store';
-import { Provider } from 'react-redux';
-import { Zustand } from './components/Zustand';
-import { CounterContextProvider } from './stores/context/store';
-import { Context } from './components/Context';
+import "./App.css";
+import { ReduxToolKit } from "./components/ReduxToolKit";
+import { store } from "./stores/redux-tool-kit/store";
+import { Provider } from "react-redux";
+import { Zustand } from "./components/Zustand";
+import { CounterContextProvider } from "./stores/context/store";
+import { Context } from "./components/Context";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Provider store={store}>
           <ReduxToolKit />
         </Provider>
--- src/components/ReduxToolKit.js
+++ src/components/ReduxToolKit.js
@@ -4,12 +4,14 @@ import { decrement, increment } from "../stores/redux-tool-kit/slices/counter";
 export const ReduxToolKit = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter.value);
+  const increase = () => dispatch(increment());
+  const decrease = () => dispatch(decrement());
 
   return (
     <div className="redux-toolkit">
       <h1>Redux Tool Kit Container</h1>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={increase}>Increment</button>
+      <button onClick={decrease}>Decrement</button>
       <p>Counter: {counter}</p>
     </div>
   );
